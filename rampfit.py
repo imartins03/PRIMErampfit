84b41cf6ac72a6c07064a3b0c09445ca602663eb
Bela Martins <imartins@terpmail.umd.edu>
2024-05-22 15:48:46 -0400

    second

--- rampfit.py
+++ rampfit.py
@@ -31,10 +31,21 @@ image2 = fits.getdata(r'D:\NLC\C1\01124972C1_ircc.fits')
 image3 = fits.getdata(r'D:\NLC\C1\01124974C1_ircc.fits')
 image4 = fits.getdata(r'D:\NLC\C1\01124975C1_ircc.fits')
 
-file_list = [image1,image2,image3,image4]
-img_cube_list = []
-superbias = 'C:\\PycharmProjects\\PRIMErampfit\\IRRC_calfiles\\super_biasC1.fits.ramp.20231012'
+frame_list = [image1,image2,image3,image4]
+superbias = fits.getdata('C:\\PycharmProjects\\PRIMErampfit\\IRRC_calfiles\\super_biasC1.fits.ramp.20231012')
 calFile = r'C:\PycharmProjects\PRIMErampfit\IRRC_calfiles\irrc_weights_C1.h5'
+
+img_cube_list = []
+hdr_list = []
+for f in frame_list:
+    hdu = fits.open(f)
+    d = hdu[image_hdu].data[:, 6:]  # Assumes data is in 1st HDU of FITS and headers are present
+    h = hdu[image_hdu].header
+    hdr_list.append(h)
+    # Mask bad reference pixels
+    dcpy = d.copy()
+    dcpy[mask] = 0
+    img_cube_list.append(dcpy)
 dataIn = np.asarray(img_cube_list)
 
 def irrc_correct_frame(image, superbias, calFile,multiThread:bool=True, externalPixelFlags:np.ndarray=None, superbias_corrected:bool=True):
@@ -49,11 +60,15 @@ def irrc_correct_frame(image, superbias, calFile,multiThread:bool=True, external
     dataIRRC = apply_in_memory(dataIn_tmp, alpha, gamma, zeta, multiThread, externalPixelFlags, superbias_corrected=True)+superbias
     return dataIRRC
 
-def get_ramp_cds(file_list,superbias, calFile,multiThread:bool=True, externalPixelFlags:np.ndarray=None, superbias_corrected:bool=True):
-    cds = irrc_correct_frame(file_list[-1]-file_list[0])
-    fits.writeto('cds_file', cds, overwrite=True)
+def get_ramp_cds(frame_list,superbias, calFile,multiThread=True, externalPixelFlags:np.ndarray=None, superbias_corrected:bool=True):
+    cds = irrc_correct_frame((frame_list[-1]-frame_list[0]),superbias,calFile, multiThread, externalPixelFlags, superbias_corrected)
+    fits.writeto('cds_file.fits', cds, overwrite=True)
 
-print(outfile)
+corrected_frame = irrc_correct_frame(dataIn, superbias, calFile)
+fits.writeto('corrected_frame_image1.fits', corrected_frame[0], overwrite=True)
+
+
+
 #%%
 print(irrc_correct_frame(image1,superbias,calFile))
 
