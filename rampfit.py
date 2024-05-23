81b9e2075c0de2a324ba240786d8602624c036df
imartins03 <163022489+imartins03@users.noreply.github.com>
2024-05-23 14:16:33 -0400

    Update rampfit.py

--- rampfit.py
+++ rampfit.py
@@ -10,6 +10,7 @@ from astropy.io import fits
 import numpy as np
 from irrc.apply_correction import _load_coeffs_from_file, apply_in_memory
 from irrc.util import destripe
+import matplotlib.pyplot as plt
 
 
 
@@ -32,23 +33,35 @@ image3 = fits.getdata(r'D:\NLC\C1\01124974C1_ircc.fits')
 image4 = fits.getdata(r'D:\NLC\C1\01124975C1_ircc.fits')
 
 frame_list = [image1,image2,image3,image4]
-superbias = fits.getdata('C:\\PycharmProjects\\PRIMErampfit\\IRRC_calfiles\\super_biasC1.fits.ramp.20231012')
+super_bias = fits.getdata('C:\\PycharmProjects\\PRIMErampfit\\IRRC_calfiles\\super_biasC1.fits.ramp.20231012')
 calFile = r'C:\PycharmProjects\PRIMErampfit\IRRC_calfiles\irrc_weights_C1.h5'
 
-img_cube_list = []
-hdr_list = []
-for f in frame_list:
-    hdu = fits.open(f)
-    d = hdu[image_hdu].data[:, 6:]  # Assumes data is in 1st HDU of FITS and headers are present
-    h = hdu[image_hdu].header
-    hdr_list.append(h)
-    # Mask bad reference pixels
-    dcpy = d.copy()
-    dcpy[mask] = 0
-    img_cube_list.append(dcpy)
-dataIn = np.asarray(img_cube_list)
+maskFile = fits.getdata(r'C:\PycharmProjects\PRIMErampfit\IRRC_calfiles\C1_bad_ref_pix_mask.fits')
+mask = maskFile>0
 
-def irrc_correct_frame(image, superbias, calFile,multiThread:bool=True, externalPixelFlags:np.ndarray=None, superbias_corrected:bool=True):
+supercpy = super_bias.copy()
+supercpy[mask[:,:4096]] = 0
+
+
+
+# img_cube_list = []
+# hdr_list = []
+# for f in frame_list:
+#     hdu = fits.open(f)
+#     d = hdu[image_hdu].data[:, 6:]  # Assumes data is in 1st HDU of FITS and headers are present
+#     h = hdu[image_hdu].header
+#     hdr_list.append(h)
+#     # Mask bad reference pixels
+#     dcpy = d.copy()
+#     dcpy[mask] = 0
+#     img_cube_list.append(dcpy)
+# dataIn = np.asarray(img_cube_list)
+
+
+def irrc_correct_frame(
+        dataIn, superbias, calFile, multiThread:bool=True, externalPixelFlags:np.ndarray=None,
+        superbias_corrected:bool=True
+):
     """Returns image cube corrected using IRRC in memory, given calibration file."""
 
     #Grab coefficients from calFile
@@ -57,29 +70,41 @@ def irrc_correct_frame(image, superbias, calFile,multiThread:bool=True, external
     dataIn_tmp = destripe(dataIn[None,:,:])
     dataIn_tmp[:,:,:4096] = dataIn_tmp[:,:,:4096]-superbias[None,:,:]
     #Apply to image cube (after correction add superbias back in)
-    dataIRRC = apply_in_memory(dataIn_tmp, alpha, gamma, zeta, multiThread, externalPixelFlags, superbias_corrected=True)+superbias
+    dataIRRC = apply_in_memory(dataIn_tmp, alpha, gamma, zeta, multiThread, externalPixelFlags,
+                               superbias_corrected)+superbias
     return dataIRRC
 
-def get_ramp_cds(frame_list,superbias, calFile,multiThread=True, externalPixelFlags:np.ndarray=None, superbias_corrected:bool=True):
-    cds = irrc_correct_frame((frame_list[-1]-frame_list[0]),superbias,calFile, multiThread, externalPixelFlags, superbias_corrected)
-    fits.writeto('cds_file.fits', cds, overwrite=True)
+def irrc_correct_frame_file(filename,superbias, calFile,multiThread:bool=True, externalPixelFlags:np.ndarray=None,
+                            superbias_corrected:bool=True):
+    dataIn = fits.getdata(filename)
+    print(dataIn.shape)
+    dataIn = dataIn[:, 6:]
+    dcpy = dataIn.copy()
+    dcpy[mask]=0
+    print(dataIn.shape)
+    return irrc_correct_frame(dcpy, superbias, calFile,multiThread, externalPixelFlags, superbias_corrected)
 
-corrected_frame = irrc_correct_frame(dataIn, superbias, calFile)
-fits.writeto('corrected_frame_image1.fits', corrected_frame[0], overwrite=True)
-
-
-
+plt.show()
+print(irrc_correct_frame_file(r'D:\NLC\C1\01124973C1.fits.fz',supercpy,calFile))
 #%%
-print(irrc_correct_frame(image1,superbias,calFile))
+file_list = [r'D:\NLC\C1\01124972C1.fits.fz',r'D:\NLC\C1\01124973C1.fits.fz']
+#,r'D:\NLC\C1\01124974C1.fits.fz',r'D:\NLC\C1\01124975C1.fits.fz']
+
+def get_ramp_cds(frame_list,superbias, calFile,multiThread=True, externalPixelFlags:np.ndarray=None,
+                 superbias_corrected:bool=True):
+    image1 = irrc_correct_frame_file(frame_list[0], superbias, calFile, multiThread, externalPixelFlags, superbias_corrected)
+    image2 = irrc_correct_frame_file(frame_list[-1], superbias, calFile, multiThread, externalPixelFlags, superbias_corrected)
+    cds = image2-image1
+    f = frame_list[0]+".cds.fits"
+    fits.writeto(f, cds, overwrite=True)
+    return f
+
+print(get_ramp_cds(file_list,supercpy,calFile,superbias_corrected=True))
+#get_ramp_cds(file_list,supercpy,calFile)
 
 
-
-#def (image1,)
- #   np.linearregression
-
-#def (image1,)
- #   return
-
+# corrected_frame = irrc_correct_frame(dataIn, superbias, calFile)
+# fits.writeto('corrected_frame_image1.fits', corrected_frame[0], overwrite=True)
 
 #%%
 
@@ -90,7 +115,76 @@ print(irrc_correct_frame(image1,superbias,calFile))
 #z = 1894
 #q = math.log2(z)
 
+#make a new function but ramp reduced
+#same inputs and write a file at the end, but instead of subtraction between teo frames do a linear fit of the frames, use linear regression numpy function
+
+#make a loop to call out/generate images, create a linear fit, and then use np.poly fit (with the 1 so its a 1 degreee polynomial) to create fit)
+
+full_file_list = [r'D:\NLC\C1\01124972C1.fits.fz',r'D:\NLC\C1\01124973C1.fits.fz',r'D:\NLC\C1\01124974C1.fits.fz',r'D:\NLC\C1\01124975C1.fits.fz']
+
+# def process_files(frame_list,superbias, calFile,multiThread=True, externalPixelFlags:np.ndarray=None):
+#     for i in range(len(full_file_list)):
+#         frame = full_file_list[i]
+#
+#         output_file = frame
+#
+#         cor_img = irrc_correct_frame_file(output_file,superbias,calFile,mask)
+#         outpt = frame + ".cor.fits"
+#         fits.writeto(outpt,cor_img,overwrite=True)
+#         print(f'The output is saved to {outpt}')
+
+def process_files(frame_list,superbias, calFile,multiThread=True, externalPixelFlags:np.ndarray=None):
+    for frame in frame_list:
+        # frame = full_file_list[i]
+        #
+        # output_file = frame
+        print('number:',frame)
+
+        out_img = irrc_correct_frame_file(frame,superbias,calFile, multiThread, externalPixelFlags)
+        outpt = frame + ".out.fits"
+        fits.writeto(outpt,out_img,overwrite=True)
+        # print(f'The output is saved to {outpt}')
+
+print(frame_list)
+print(process_files(full_file_list,supercpy,calFile))
+
+#%%
+
+def get_ramp_slope(frame_list,superbias, calFile, mask):
+    slopes = []
+    corrected_images = []
+    for frame in frame_list:
+        out_img = irrc_correct_frame_file(frame,superbias,calFile)
+        y = out_img.flatten()  # ask if this works
+        x = np.arange(len(y))  #???
+        coefficients = np.polyfit(x,y,1)
+        slope = coefficients[0]   #the slope is just the coefficient of the first degree term (y=mx+b)
+        slopes.append(slope)   #adding the slopes to the list
+
+        #To create a linear fit line, polyval evaluates specific values for coefficients, polyfit fits and return the actual coefficients
+        fit = np.polyval(coefficients,x)
+        fit_image = fit.reshape(out_img.shape)  #tried to fit it back to the image here (instead of flattened)
 
 
+        #THIS IS WRONG
+        cor_img = slopes/out_img
+        corrected_images.append(cor_img)
 
+        return slopes,corrected_images
+
+slopes,corrected_images = get_ramp_slope(full_file_list,supercpy,calFile,mask)
+print(f'slopes={slopes},cor_imgs={corrected_images}')
+#%%
+
+#NEED TO MAKE SURE ALL IMAGES DOWNLOAD AND NOT JUST ONE
+def corrected_files(frame_list,superbias, calFile,multiThread=True, externalPixelFlags:np.ndarray=None):
+    slopes, corrected_images = get_ramp_slope(frame_list, superbias, calFile, mask)
+    for frame,cor_img in zip(frame_list,corrected_images):
+        print('number processing:',frame)
+        
+        # img = get_ramp_slope(frame_list,superbias, calFile, mask)
+        output = frame + ".cor.fits"
+        fits.writeto(output, cor_img, overwrite=True)
+
+print(corrected_files(full_file_list,supercpy,calFile))
 
