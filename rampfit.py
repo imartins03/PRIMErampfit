7ba2e83e482b2f4455fb6b75821ae7c8bcf93c42
imartins03 <imartins@terpmail.umd.edu>
2024-06-07 13:17:37 -0400

    rampfit with his attempt

--- rampfit.py
+++ rampfit.py
@@ -77,7 +77,7 @@ def plot_temperatures(frame_list):
     print(fits.getheader(frame_list[0]))
     temps = [fits.open(f)[1].header['TEMPASIC'] for f in frame_list]
     plt.plot(temps)
-    plt.show()
+    # plt.show()
 
 plot_temperatures(file_list)
 def irrc_correct_frame_file(filename,superbias, calFile,multiThread:bool=True, externalPixelFlags:np.ndarray=None,
@@ -90,7 +90,7 @@ def irrc_correct_frame_file(filename,superbias, calFile,multiThread:bool=True, e
     print(dataIn.shape)
     return irrc_correct_frame(dcpy, superbias, calFile,multiThread, externalPixelFlags, superbias_corrected)
 
-plt.show()
+# plt.show()
 print(irrc_correct_frame_file(r'D:\NLC\C1\01124973C1.fits.fz',supercpy,calFile))
 def get_ramp_cds(frame_list,superbias, calFile,multiThread=True, externalPixelFlags:np.ndarray=None,
                  superbias_corrected:bool=True):
@@ -168,7 +168,7 @@ def get_ramp_slope(frame_list,superbias, calFile, mask, slc=((4,4092), (4,4092))
     sum_sat = sat_pix.sum(axis=0)
     bad_pix = sum_sat > 0
     y[:, bad_pix] = 0
-    coefficients, cov_mat = np.polyfit(x[:-50], y[:-50,:], degrees, cov=True)
+    coefficients, cov_mat = np.polyfit(x, y, degrees, cov=True)
     # for i in range(y.shape[1]):
     #     w=unsat_pix[:, i]
     #     if w.sum() < degrees+1:
@@ -204,128 +204,37 @@ def get_ramp_slope(frame_list,superbias, calFile, mask, slc=((4,4092), (4,4092))
     return coeff_images, cov_matrices
 
 print('get_ramp_slope')
-coeff_images = get_ramp_slope(full_file_list,supercpy,calFile,mask,degrees=6)
+coeff_images = get_ramp_slope(full_file_list,supercpy,calFile,mask,degrees=1)
+
+#%%
+def val(frame_list,superbias, calFile, mask, slc=((4,4092), (4,4092)),degrees=4, saturation=50000):
+    for frame in frame_list:
+        coeff_data = fits.getdata(full_file_list[0].replace('.fits.fz', f'.img_cb_1deg.fits'))
+        val = np.polyval(degrees,coeff_data)
+        if slc is not None:
+            val = val[:, slc[0][0]:slc[0][1], slc[1][0]:slc[1][1]]
+            val_cube = np.array([fits.getdata(value) for value in val])
+            return val, val_cube
+
+val_cube = val(full_file_list,supercpy,calFile,mask,degrees=1)
+residuals_cube = coeff_images - val_cube #this didn't work because shapes are different
 
 
-# image1 = fits.getdata(r'D:\NLC\C1\01124973C1_ircc.fits')
-# image2 = fits.getdata(r'D:\NLC\C1\01124972C1_ircc.fits')
-#
-# print(image1.shape)
-# print(image2.shape)
-#
-# im_new = image1-image2
-#
-# fits.writeto('subtracted1_image.fits',im_new,overwrite=True)
-# print(outfile)
-#
-# file_format = r'D:\NLC\C1\{0:08d}C1.fits.fz'
-# # r = (1124972, 1125497)
-# r = (1124972, 1124972+4)
-#
-# file_list = [file_format.format(n) for n in range(*r)]
-# full_file_list = file_list
-# print(len(file_list))
-#
-# super_bias = fits.getdata('C:\\PycharmProjects\\PRIMErampfit\\IRRC_calfiles\\super_biasC1.fits.ramp.20231012')
-# calFile = r'C:\PycharmProjects\PRIMErampfit\IRRC_calfiles\irrc_weights_C1.h5'
-#
-# maskFile = fits.getdata(r'C:\PycharmProjects\PRIMErampfit\IRRC_calfiles\C1_bad_ref_pix_mask.fits')
-# mask = maskFile>0
-#
-# supercpy = super_bias.copy()
-# supercpy[mask[:,:4096]] = 0
+fits.writeto('residuals.fits', residuals_cube, overwrite=True)
+plt.figure()
 
-# def irrc_correct_frame(
-#         dataIn, superbias, calFile, multiThread:bool=True, externalPixelFlags:np.ndarray=None,
-#         superbias_corrected:bool=True
-# ):
-#     """Returns image cube corrected using IRRC in memory, given calibration file."""
-#
-#     #Grab coefficients from calFile
-#     alpha, gamma, zeta = _load_coeffs_from_file(calFile)
-#     #De-stripe input (subtract off superbias)
-#     dataIn_tmp = destripe(dataIn[None,:,:])
-#     dataIn_tmp[:,:,:4096] = dataIn_tmp[:,:,:4096]-superbias[None,:,:]
-#     #Apply to image cube (after correction add superbias back in)
-#     dataIRRC = apply_in_memory(dataIn_tmp, alpha, gamma, zeta, multiThread, externalPixelFlags,
-#                                superbias_corrected)+superbias
-#     return dataIRRC
-#
-# def irrc_correct_frame_file(filename,superbias, calFile,multiThread:bool=True, externalPixelFlags:np.ndarray=None,
-#                             superbias_corrected:bool=True):
-#     dataIn = fits.getdata(filename)
-#     print(dataIn.shape)
-#     dataIn = dataIn[:, 6:]
-#     dcpy = dataIn.copy()
-#     dcpy[mask]=0
-#     print(dataIn.shape)
-#     return irrc_correct_frame(dcpy, superbias, calFile,multiThread, externalPixelFlags, superbias_corrected)
-#
-# plt.show()
-# print(irrc_correct_frame_file(r'D:\NLC\C1\01124973C1.fits.fz',supercpy,calFile))
-# def get_ramp_cds(frame_list,superbias, calFile,multiThread=True, externalPixelFlags:np.ndarray=None,
-#                  superbias_corrected:bool=True):
-#     image1 = irrc_correct_frame_file(frame_list[0], superbias, calFile, multiThread, externalPixelFlags, superbias_corrected)
-#     image2 = irrc_correct_frame_file(frame_list[-1], superbias, calFile, multiThread, externalPixelFlags, superbias_corrected)
-#     cds = image2-image1
-#     f = frame_list[0]+".cds.fits"
-#     fits.writeto(f, cds, overwrite=True)
-#     return f
-# #%%
-# def process_files(frame_list,superbias, calFile,multiThread=True, externalPixelFlags:np.ndarray=None):
-#     for frame in frame_list:
-#         # frame = full_file_list[i]
-#         #
-#         # output_file = frame
-#         print('number:',frame)
-#
-#         out_img = irrc_correct_frame_file(frame,superbias,calFile, multiThread, externalPixelFlags)
-#         outpt = frame + ".out.fits"
-#         fits.writeto(outpt,out_img,overwrite=True)
+summed_residuals = np.sum(residuals_cube, axis=0)
 
-# def get_ramp_slope(frame_list,superbias, calFile, mask, slc=((4,4092), (4,4092)),degrees=4):  # ((y1,y2), (x1,x2))
-#     slopes = []
-#     corrected_images = []
-#     y=[]
-#     for frame in frame_list:
-#         out_img = irrc_correct_frame_file(frame,superbias,calFile)
-#         if slc is not None:
-#             print('slc')
-#             print(out_img.shape)
-#             out_img = out_img[:, slc[0][0]:slc[0][1], slc[1][0]:slc[1][1]]
-#             print(out_img.shape)
-#
-#     #ISSUE HERE
-#     saturation_mask = out_img > 20000
-#     out_img[saturation_mask] = np.nan
-#
-#     y.append(out_img)
-#     x = np.arange(len(y))  #???
-#     y = np.asarray(y)
-#     y = y.reshape((x.shape[0],-1))
-#
-#     # slope = coefficients[0]   #the slope is just the coefficient of the first degree term (y=mx+b)
-#
-#     coeff_images = []
-#     cov_matrices = []
-#     coefficients, cov_mat = np.ma.polyfit(x, y, degrees,cov=True)
-#
-#     #ISSUE HERE WITH THE COVARIANCE
-#     for coeff in coefficients:
-#         coeff_image = coeff.reshape(out_img.shape)
-#         coeff_images.append(coeff_image)
-#         cov_mat_img = coeff.reshape(out_img.shape)
-#         cov_matrices.append(cov_mat_img)
-#
-#     coeff_images = np.stack(coeff_images,axis=0)
-#     cov_matrices = np.stack(cov_matrices,axis=0)
-#
-#     fits.HDUList([fits.PrimaryHDU(coeff_images)]).writeto(frame_list[0].replace('.fits.fz', f'.img_cb_{degrees}deg.fits'), overwrite=True)
-#     fits.HDUList([fits.PrimaryHDU(cov_matrices)]).writeto(frame_list[0].replace('.fits.fz', f'.pcov_cb_{degrees}deg.fits'), overwrite=True)
-#
-#     return coeff_images, cov_matrices
-#
-# coeff_images,cov_matrices = get_ramp_slope(full_file_list,supercpy,calFile,mask,degrees=1)
+# Save the summed residuals to a new FITS file
+fits.writeto('summed_residuals.fits', summed_residuals, overwrite=True)
 
+#ravel flattens pixels into a single array so we can easily put it into a histogram,although im not sure if this is what we want here
+plt.hist(summed_residuals.ravel())
+plt.title('Histogram of Summed Residuals')
+plt.xlabel('Residual Value')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.show()
 
+#%%
 
