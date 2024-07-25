02eec9af32f176ac1c3f87ab5d5ef0d87ed8530d
Bela Martins <imartins@terpmail.umd.edu>
2024-07-25 10:23:27 -0400

    both plots

--- adu_vs_frame_plot_poly.py
+++ adu_vs_frame_plot_poly.py
@@ -1,48 +1,54 @@
 from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-# Path to y_cube FITS file
-# y_cube_path = r'D:\NLC\C1\y_cube_100im.fits'
-# y_cube = fits.getdata(y_cube_path)
-# y = y_cube.reshape(y_cube.shape[0], y_cube.shape[2], y_cube.shape[3])
-# center_x, center_y = y.shape[1]//2, y.shape[2]//2
-# superpix_size = 256
-# half_size = superpix_size//2
+# Directory where the data files are stored
+data_directory = r'D:\NLC\C1\dif_degrees_test'
 
-residuals_cube_path = r'D:\NLC\C1\residuals_poly.fits'
-y = fits.getdata(residuals_cube_path)
-center_x, center_y = y.shape[1]//2, y.shape[2]//2
-superpix_size = 256
-half_size = superpix_size//2
+# Initialize lists to store median values and frame numbers
+median_values_all_deg = []
 
-# fit_cube_path = r'D:\NLC\C1\fit_cube_poly.fits'
-# y = fits.getdata(fit_cube_path)
-# center_x, center_y = y.shape[1]//2, y.shape[2]//2# Initialize lists to store median values and frame numbers
-# superpix_size = 256median_values = []
-# half_size = superpix_size//2frame_numbers = np.arange(y.shape[0])
+# Loop over all 10 degrees
+for degree in range(1, 11):
+    # Construct the file path for residuals data
+    residuals_cube_path = os.path.join(data_directory, f'residuals_poly_{degree}deg.fits')
 
+    # Load the data
+    y = fits.getdata(residuals_cube_path)
+    center_x, center_y = y.shape[1] // 2, y.shape[2] // 2
+    superpix_size = 256
+    half_size = superpix_size // 2
 
-median_values = []
-frame_numbers = np.arange(y.shape[0])
+    median_values = []
 
-# Iterate over each frame
-for frame_n in range(y.shape[0]):
-    frame = y[frame_n]
+    # Iterate over each frame
+    for frame_n in range(y.shape[0]):
+        frame = y[frame_n]
 
-    # Extract 256x256 square from the center
-    superpix = frame[center_x - half_size:center_x + half_size,
-                     center_y - half_size:center_y + half_size]
+        # Extract 256x256 square from the center
+        superpix = frame[center_x - half_size:center_x + half_size,
+                   center_y - half_size:center_y + half_size]
 
-    median_value = np.median(superpix)
-    median_values.append(median_value)
+        median_value = np.median(superpix)
+        median_values.append(median_value)
 
-# Plot the median values as a function of frame number
-plt.plot(frame_numbers, median_values, marker='o', linestyle='-', color='black')
-plt.title('Median of 256x256 super pixel from Center as a Function of Frame Number (polyfit)')
-plt.xlabel('Frame Number')
-plt.ylabel('Median Value of 256x256 superpixel')
-plt.grid(True)
-plt.savefig(r'D:\NLC\C1\median_superpix_plot_poly.png')  # Save the plot
-plt.show()
+    # Plot the median values as a function of frame number
+    plt.figure()
+    plt.plot(np.arange(len(median_values)), median_values, marker='o', linestyle='-', color='black')
+    plt.title(f'Median of 256x256 super pixel ({degree} deg fit) from Center as a Function of Frame Number (polyfit)')
+    plt.xlabel('Frame Number')
+    plt.ylabel('Median Value of 256x256 superpixel')
+    plt.grid(True)
+
+    # Save the plot
+    plot_filename = os.path.join(data_directory, f'median_superpix_plot_poly_{degree}deg.png')
+    plt.savefig(plot_filename)
+    plt.show()
+    plt.close()  # Close the figure to release memory
+
+    # Append median values for this degree to the list
+    median_values_all_deg.append(median_values)
+
+#%%
 
