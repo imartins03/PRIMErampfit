b813e4ac975a8ca128d1ab6e0ba2c59cb6a32949
Bela Martins <imartins@terpmail.umd.edu>
2024-07-31 10:56:13 -0400

    both plots

--- rampfit_superpixels.py
+++ rampfit_superpixels.py
@@ -1,6 +1,7 @@
 from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 # Paths
 super_bias_path = 'IRRC_calfiles\\super_biasC1.fits.ramp.20231012'
@@ -28,7 +29,14 @@ def evaluate_poly_array(coeffs, a_array, poly_type='power'):
     return np.asarray(output_arrays)
 
 
-def generate_fit_cube(region, degrees, saturation=50000):
+def generate_fit_cube(center, degrees, saturation=50000):
+    half_size = 128  # Since size=256, half_size is 128
+    y_start = center[0] - half_size
+    y_end = center[0] + half_size
+    x_start = center[1] - half_size
+    x_end = center[1] + half_size
+    region = (y_start, y_end, x_start, x_end)
+
     y_cube = fits.getdata(y_cube_path)[1:n_frames]   #I did this slicing so that it cuts out the first frame
 
     x,_, y, z = y_cube.shape
@@ -46,48 +54,55 @@ def generate_fit_cube(region, degrees, saturation=50000):
     # Perform polynomial fitting
     coefficients, _ = np.polyfit(time, y, degrees, cov=True)
     fit_coeff = coefficients.reshape(degrees + 1, region[1] - region[0], region[3] - region[2]) #region[1]-region[0] in this case will be 256 because thats the size of the superpixel (degrees+1,256,256)
-    fits.writeto(fit_coeff_path + f'region_{region[0]}_{region[2]}_{degrees}.fits', fit_coeff, overwrite=True)
+    center_str = f"{center[0]}_{center[1]}"
+    fits.writeto(fit_coeff_path + f'center_{center_str}_{degrees}_noframe1.fits', fit_coeff, overwrite=True)
 
     fit_cube = evaluate_poly_array(np.flip(coefficients, axis=0), time)
     fit_cube = fit_cube.reshape(len(time), region[1] - region[0], region[3] - region[2])
-    fits.writeto(fit_cube_base_path + f'region_{region[0]}_{region[2]}_{degrees}.fits', fit_cube, overwrite=True)
+    fits.writeto(fit_cube_base_path + f'center_{center_str}_{degrees}_noframe1.fits', fit_cube, overwrite=True)
+
     return fit_cube
 
-def process_superpixels(centers, size=256, degrees=1):  #this function is used so I can make a loop to do all 3 regions at once
-    half_size = size // 2
-
-    # Define regions
-    regions = []
-    for center in centers:
-        y_start = center[0] - half_size
-        y_end = center[0] + half_size
-        x_start = center[1] - half_size
-        x_end = center[1] + half_size
-        regions.append((y_start, y_end, x_start, x_end))
-
+def process_superpixels(centers, size=256, degrees=1):
     fit_results = {}
-    for i, region in enumerate(regions):
-        fit_cube = generate_fit_cube(region, degrees)
-        fit_results[f'Region_{i}'] = fit_cube #this just makes it easier to see the results as variables for each region, basically makes a dictionary fit results that stores the values for each region
-        print('fit_cube',fit_cube)
+    for center in centers:
+        fit_cube = generate_fit_cube(center, degrees)
+        center_str = f"{center[0]}_{center[1]}"
+        fit_results[f'Center_{center_str}'] = fit_cube
+        print(f'fit_cube for center {center}:', fit_cube)
 
     return fit_results
 
 
 #I just did this to give me a visual representation of where the centers of the three superpixels are
 def plot_superpixels_centers(image_size, centers, size=256):
-    plt.imshow(np.zeros(image_size), cmap='gray', origin='lower')
+    fig, ax = plt.subplots()
+    ax.imshow(np.zeros(image_size), cmap='gray', origin='lower')
 
-    # Plot centers as dots
+    half_size = size // 2
+
+    # Plot centers as dots and squares
     for center in centers:
-        plt.plot(center[1], center[0], 'ro', markersize=10)  # Red dots for centers
+        # Plot the center point
+        ax.plot(center[1], center[0], 'ro', markersize=10)  # Red dots for centers
 
-    plt.title('Superpixel Centers')
-    plt.xlabel('X Coordinate')
-    plt.ylabel('Y Coordinate')
-    plt.grid(True)
+        # Define the square's position
+        rect = patches.Rectangle(
+            (center[1] - half_size, center[0] - half_size),  # Bottom-left corner
+            size,  # Width
+            size,  # Height
+            linewidth=1,  # Line width
+            edgecolor='r',  # Edge color
+            facecolor='none'  # No fill color
+        )
+        # Add the rectangle to the plot
+        ax.add_patch(rect)
+
+    ax.set_title('Superpixel Centers')
+    ax.set_xlabel('X Coordinate')
+    ax.set_ylabel('Y Coordinate')
+    ax.grid(True)
     plt.show()
-
 
 centers = [(2048, 2048), (3072,2048), (1024,2048)]  # Centers of the superpixels
 degrees = 6
