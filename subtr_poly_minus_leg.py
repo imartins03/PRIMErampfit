4ab934d6f93bf5c955710c4ff8d0a05f49ba0ca5
Bela Martins <imartins@terpmail.umd.edu>
2024-08-14 15:20:31 -0400

    working on saving full coeff images

--- subtr_poly_minus_leg.py
+++ subtr_poly_minus_leg.py
@@ -1,3 +1,12 @@
 import numpy as np
+from astropy.io import fits
 
-coeff_im_deg_1_poly = fits.getdata()
+coeff_deg1_239_poly = fits.getdata(r'F:\leftover_C1_dif_degrees_test_rampfit\239_frames\unweighted\fit_coeff_poly_1deg_239frames_noframe1.fits')
+coeff_deg1_239_leg = fits.getdata(r"F:\legfit\239_frames_unweighted\coefficients_leg_239frames_1deg_final_vst_unweighted.fits")
+
+def coeff_res():
+    res = coeff_deg1_239_poly - coeff_deg1_239_leg
+    print(res)
+    return res
+
+coeff_res()
